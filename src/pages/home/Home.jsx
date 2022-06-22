4380183de09f7abbeaacc49b57a60e8e21929a3d
Sandesh Mankar <mankarsandesh11@gmail.com>
2022-06-22 17:51:55 +0700

    [FEAT] Page and files creaed

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import './home.css'
+
+const Home = () => {
+	return (
+		<div>
+			<div className="homeContainer">
+				<h1 className="homeTitle">Browse by property type</h1>
+
+				<h1 className="homeTitle">Homes guests love</h1>
+			</div>
+		</div>
+	)
+}
+
+export default Home
