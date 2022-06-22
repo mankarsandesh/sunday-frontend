80a4d8fd000bfd66b4b6c025f90bfb18a18f6489
Sandesh Mankar <mankarsandesh11@gmail.com>
2022-06-22 21:09:10 +0700

    [FEAT] Basic Changes on Header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import './navbar.css'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+	const navigate = useNavigate()
+
+	const homePage = () => {
+		navigate('/')
+	}
+
 	return (
 		<div className="navbar">
 			<div className="navContainer">
-				<span className="logo">Sunday Travel</span>
+				<span className="logo" onClick={() => homePage()}>
+					Sunday Travel
+				</span>
 				<div className="navItems">
 					<button className="navButton">Register</button>
 					<button className="navButton">Login</button>
