eb57a89232f0dbf073dc609c199373be63caacf6
Sandesh Mankar <mankarsandesh11@gmail.com>
2022-06-23 10:11:20 +0700

    [FEAT] Home Pages Design fixed

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -1,16 +1,22 @@
-import "./mailList.css"
+import './mailList.css'
 
 const MailList = () => {
-  return (
-    <div className="mail">
-      <h1 className="mailTitle">Save time, save money!</h1>
-      <span className="mailDesc">Sign up and we'll send the best deals to you</span>
-      <div className="mailInputContainer">
-        <input type="text" placeholder="Your Email" />
-        <button>Subscribe</button>
-      </div>
-    </div>
-  )
+	return (
+		<div className="mail">
+			<h1 className="mailTitle">Save time, save money!</h1>
+			<span className="mailDesc">
+				Sign up and we'll send the best deals to you
+			</span>
+			<div className="mailInputContainer">
+				<input type="text" placeholder="Your Email" />
+				<button>Subscribe</button>
+			</div>
+			<div>
+				<input type="checkbox" placeholder="" />
+				Send me a link to get the FREE Booking.com app!
+			</div>
+		</div>
+	)
 }
 
 export default MailList
