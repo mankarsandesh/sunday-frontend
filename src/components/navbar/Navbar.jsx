1545f39599c1bb227d7974a459cf3d1c4cbb6896
Sandesh Mankar <mankarsandesh11@gmail.com>
2022-06-23 09:22:04 +0700

    [FEAT] Header add items and change menu css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import './navbar.css'
 import { useNavigate } from 'react-router-dom'
+import { faCircleQuestion, faLanguage } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
 	const navigate = useNavigate()
@@ -12,11 +14,20 @@ const Navbar = () => {
 		<div className="navbar">
 			<div className="navContainer">
 				<span className="logo" onClick={() => homePage()}>
-					Sunday Travel
+					Sunday.com
 				</span>
 				<div className="navItems">
+					<button className="listIcon">LAK</button>
+					<button className="listIcon">
+						<FontAwesomeIcon icon={faLanguage} />
+					</button>
+					<button className="listIcon">
+						<FontAwesomeIcon icon={faCircleQuestion} />
+					</button>
+
+					<button className="listButton">List your Property</button>
 					<button className="navButton">Register</button>
-					<button className="navButton">Login</button>
+					<button className="navButton">Sign In</button>
 				</div>
 			</div>
 		</div>
